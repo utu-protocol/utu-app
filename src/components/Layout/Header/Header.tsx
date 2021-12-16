1274e992e0c2788993cdfd775ce4d48d0291fff9
Raphael Karanja <raphael@utu.io>
2021-12-16 14:18:38 +0300

    refactor: ensure components are responsive to different screen sizes

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from "react-router-dom";
 import "./header.scss";
 import logo from "../../../assets/images/utu-light-logo.svg";
@@ -6,14 +6,21 @@ import avatar from "../../../assets/images/driver.png";
 import UserAvatar from "./partials/UserAvatar/UserAvatar";
 
 const Header = () => {
+    const [responsive, setResponsive] = useState(false);
     return (
-        <header className="header">
+        <header className={responsive ? "header responsive" : "header"}>
             <nav className="menu">
                 <div className="menu-item">
                     <Link to="/"><img src={logo} alt="Utu Logo" height="40"/></Link>
                 </div>
                 <div className="menu-item">
-                    <UserAvatar token="020a4b1a-5dcf-11ec-bf63-0242ac130002" logo={avatar} />
+                    <UserAvatar token="020a4b1a-5dcf-11ec-bf63-0242ac130002" logo={avatar}/>
+                </div>
+
+                <div className="menu-item icon" onClick={() => setResponsive(!responsive)}>
+                    <div>
+                        &#9776;
+                    </div>
                 </div>
             </nav>
         </header>
