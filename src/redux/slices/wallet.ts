1aa3311e39c6944053b7b4cb35aabf9854dbed4a
musayann <musayann@octangrp.com>
2021-12-20 11:26:25 +0200

    feat: add provider listeners

--- src/redux/slices/wallet.ts
+++ src/redux/slices/wallet.ts
@@ -1,8 +1,9 @@
-import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import WalletConnectProvider from "@walletconnect/web3-provider";
 import { AppThunk, RootState } from "../store";
 import Web3Modal from "web3modal";
 import { providers } from "ethers";
+import Web3 from "web3";
 
 const INFURA_ID = "460f40a260564ac4a4f4b3fffb032dad";
 
@@ -16,7 +17,7 @@ const providerOptions = {
 };
 
 let web3Modal: any;
-let provider: any;
+let web3: any;
 
 if (typeof window !== "undefined") {
   web3Modal = new Web3Modal({
@@ -31,21 +32,103 @@ if (web3Modal) {
 }
 
 export interface WalletState {
-  provider?: any;
-  web3Provider?: any;
   address?: string | null;
   chainId?: number | null;
+  networkId?: number | null;
 }
 
 const initialState: WalletState = {
-  provider: null,
-  web3Provider: null,
   address: null,
   chainId: null,
+  networkId: null,
 };
 
-export const connectWallet = createAsyncThunk("wallet/connect", async () => {
-  provider = await web3Modal.connect();
+export const initWeb3 = (provider: any) => {
+  const web3: any = new Web3(provider);
+
+  web3.eth.extend({
+    methods: [
+      {
+        name: "chainId",
+        call: "eth_chainId",
+        outputFormatter: web3.utils.hexToNumber,
+      },
+    ],
+  });
+
+  return web3;
+};
+
+export const walletSlice = createSlice({
+  name: "wallet",
+  initialState,
+  // The `reducers` field lets us define reducers and generate associated actions
+  reducers: {
+    // Use the PayloadAction type to declare the contents of `action.payload`
+    setWeb3Provider: (state, action: PayloadAction<WalletState>) => {
+      state.address = action.payload.address;
+      state.chainId = action.payload.chainId;
+      state.networkId = action.payload.networkId;
+    },
+    resetWeb3Provider: (state) => {
+      state = initialState;
+    },
+    setAddress: (state, action: PayloadAction<string>) => {
+      state.address = action.payload;
+    },
+    setChainId: (state, action: PayloadAction<number>) => {
+      state.chainId = action.payload;
+    },
+    setNetworkId: (state, action: PayloadAction<number>) => {
+      state.networkId = action.payload;
+    },
+  },
+});
+
+export const {
+  setWeb3Provider,
+  resetWeb3Provider,
+  setAddress,
+  setChainId,
+  setNetworkId,
+} = walletSlice.actions;
+
+// The function below is called a selector and allows us to select a value from
+// the state. Selectors can also be defined inline where they're used instead of
+// in the slice file. For example: `useSelector((state: RootState) => state.wallet.value)`
+export const selectAddress = (state: RootState) => state.wallet.address;
+
+export const initWallet = (): AppThunk => (dispatch) => {
+  if (web3Modal && web3Modal.cachedProvider) {
+    dispatch(connectWallet());
+  }
+};
+
+export const subscribeProvider =
+  (provider: any): AppThunk =>
+  async (dispatch) => {
+    if (!provider.on) {
+      return;
+    }
+    provider.on("close", () => dispatch(disconnectWallet()));
+    provider.on("accountsChanged", async (accounts: string[]) => {
+      await dispatch(setAddress(accounts[0]));
+    });
+    provider.on("chainChanged", async (chainId: number) => {
+      await dispatch(setChainId(chainId));
+      const networkId = await web3.eth.net.getId();
+      await dispatch(setNetworkId(networkId));
+    });
+
+    provider.on("networkChanged", async (networkId: number) => {
+      await dispatch(setNetworkId(networkId));
+      const chainId = await web3.eth.chainId();
+      await dispatch(setChainId(chainId));
+    });
+  };
+
+export const connectWallet = (): AppThunk => async (dispatch) => {
+  const provider = await web3Modal.connect();
 
   // We plug the initial `provider` into ethers.js and get back
   // a Web3Provider. This will add on methods from ethers.js and
@@ -55,71 +138,31 @@ export const connectWallet = createAsyncThunk("wallet/connect", async () => {
   const signer = web3Provider.getSigner();
   const address = await signer.getAddress();
 
-  const network = await web3Provider.getNetwork();
+  web3 = initWeb3(provider);
+  const networkId = await web3.eth.net.getId();
+  const chainId = await web3.eth.chainId();
+
+  dispatch(subscribeProvider(provider));
   // The value we return becomes the `fulfilled` action payload
-  return {
-    provider: {},
-    web3Provider: {},
+  const data = {
     address,
-    chainId: network.chainId,
+    chainId,
+    networkId,
   };
-});
 
-export const disconnectWallet = createAsyncThunk(
-  "wallet/disconnect",
-  async () => {
-    const provider = await web3Modal.cachedProvider;
-    await web3Modal.clearCachedProvider();
-    if (provider?.disconnect && typeof provider.disconnect === "function") {
-      await provider.disconnect();
-    }
+  dispatch(setWeb3Provider(data));
+
+  return data;
+};
+
+export const disconnectWallet = (): AppThunk => async (dispatch) => {
+  const provider = await web3Modal.cachedProvider;
+  await web3Modal.clearCachedProvider();
+  if (provider?.disconnect && typeof provider.disconnect === "function") {
+    await provider.disconnect();
   }
-);
 
-export const walletSlice = createSlice({
-  name: "wallet",
-  initialState,
-  // The `reducers` field lets us define reducers and generate associated actions
-  reducers: {
-    // Use the PayloadAction type to declare the contents of `action.payload`
-    setWeb3Provider: (state, action: PayloadAction<WalletState>) => {
-      state = {
-        ...state,
-        ...action.payload,
-      };
-    },
-    resetWeb3Provider: (state) => {
-      state = initialState;
-    },
-  },
-  extraReducers: (builder) => {
-    builder
-      .addCase(
-        connectWallet.fulfilled,
-        (state, action: PayloadAction<WalletState>) => {
-          state.address = action.payload.address;
-          state.web3Provider = action.payload.web3Provider;
-          state.chainId = action.payload.chainId;
-          state.provider = action.payload.provider;
-        }
-      )
-      .addCase(disconnectWallet.fulfilled, (state, action) => {
-        state = initialState;
-      });
-  },
-});
-
-export const { setWeb3Provider, resetWeb3Provider } = walletSlice.actions;
-
-// The function below is called a selector and allows us to select a value from
-// the state. Selectors can also be defined inline where they're used instead of
-// in the slice file. For example: `useSelector((state: RootState) => state.wallet.value)`
-export const selectAddress = (state: RootState) => state.wallet.address;
-
-export const initWallet = (): AppThunk => (dispatch, getState) => {
-  if (web3Modal && web3Modal.cachedProvider) {
-    dispatch(connectWallet());
-  }
+  dispatch(resetWeb3Provider());
 };
 
 export default walletSlice.reducer;
