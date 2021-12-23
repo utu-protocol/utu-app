2ffbe5b33ef40af1176e61247b9105675c5122ec
kimiti <kimitikevin@gmail.com>
2021-12-23 09:05:28 +0300

    add telegram api

---
+++ src/pages/Connect/partials/Telegram/TelegramApi.tsx
@@ -0,0 +1,28 @@
+import {TelegramClient} from "telegram";
+import {StringSession} from "telegram/sessions";
+
+// @ts-ignore
+import input from "input"; // npm i input
+
+
+const apiId = 123456;
+const apiHash = "123456abcdfg";
+const stringSession = new StringSession(""); // fill this later with the value from session.save()
+
+
+(async () => {
+    console.log("Loading interactive example...");
+    const client = new TelegramClient(stringSession, apiId, apiHash, {
+        connectionRetries: 5,
+    });
+    await client.start({
+        phoneNumber: async () => await input.text("Please enter your number: "),
+        password: async () => await input.text("Please enter your password: "),
+        phoneCode: async () =>
+            await input.text("Please enter the code you received: "),
+        onError: (err) => console.log(err),
+    });
+    console.log("You should now be connected.");
+    console.log(client.session.save()); // Save this string to avoid logging in again
+    await client.sendMessage("me", {message: "Hello!"});
+})();
