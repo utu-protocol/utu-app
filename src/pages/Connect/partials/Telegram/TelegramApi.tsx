c9497a64e55780d4eb1f30ec82b4d446544c7510
kimiti <kimitikevin@gmail.com>
2021-12-26 12:21:22 +0300

    telegram login success

--- src/pages/Connect/partials/Telegram/TelegramApi.tsx
+++ src/pages/Connect/partials/Telegram/TelegramApi.tsx
@@ -1,7 +1,6 @@
 import {Api, TelegramClient} from "telegram";
 import {StringSession} from "telegram/sessions";
 
-import input from "input"; // npm i input
 
 
 const apiId = 123456;
@@ -10,28 +9,25 @@ const stringSession = new StringSession(""); // fill this later with the value f
 
 // (async () => {})();
 
-export const TGApi = async (phoneNumber, password, phoneCode) => {
+export const TGApi = async (phoneNumber: any, password: any, phoneCode: any) => {
     console.log("Loading interactive example...");
     const client = new TelegramClient(stringSession, apiId, apiHash, {
         connectionRetries: 5,
     });
-    await client.start({
-        phoneNumber: async () => await phoneNumber,
-        password: async () => await password,
-        phoneCode: async () =>
-            await phoneCode,
-        onError: (err) => console.log(err),
-    });
+     await client.start({
+         phoneNumber: async () => await phoneNumber,
+         password: async () => await password,
+         phoneCode: async () => await phoneCode,
+         onError: (err) => console.log(err),
+     });
 
 
     console.log("You should now be connected.");
     console.log(client.session.save()); // Save this string to avoid logging in again
-    await client.sendMessage("me", {message: "Hello!"});
+    // await client.sendMessage("me", {message: "Hello!"});
 
     const result = await client.invoke(
-        new Api.contacts.GetContacts({
-            hash: 0,
-        })
+        new Api.contacts.GetContacts({})
     );
     console.log(result); // prints the result
 }
--- src/pages/Connect/partials/Telegram/TelegramConnect.tsx
+++ src/pages/Connect/partials/Telegram/TelegramConnect.tsx
@@ -1,59 +1,79 @@
-import React, {Fragment, useState} from "react";
+import React, { Fragment, useState } from "react";
 import TwitterConnect from "../Twitter/TwitterConnect";
 import Button from "../../../../components/Button";
 import Modal from "../../../../components/Modal/Modal";
 import ConnectHelper from "../ConnectHelper/ConnectHelper";
 import telegram from "../../../../assets/images/telegram.svg";
 
-import "./TelegramConnect.scss"
-import {TGApi} from "./TelegramApi";
+import "./TelegramConnect.scss";
+import { TGApi } from "./TelegramApi";
 
 const TelegramConnect = () => {
-    const [connectModal, setConnectModal] = useState(false);
+  const [connectModal, setConnectModal] = useState(false);
 
-    const [phoneNumber, setPhoneNumber] = useState('')
-    const [password, setPassword] = useState('')
-    const [phoneCode, setPhoneCode] = useState('')
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [password, setPassword] = useState("");
+  const [phoneCode, setPhoneCode] = useState("");
 
-    const submitHandler = (e :any) => {
-        e.preventDefault()
-        console.log(phoneNumber,password, phoneCode)
-        // TGApi(phoneNumber,password,phoneCode)
-    }
+  const submitHandler = async (e: any) => {
+    //   e.preventDefault();
+    console.log(phoneNumber, password, phoneCode);
+    await TGApi(phoneNumber, password, phoneCode);
+  };
 
+  return (
+    <Fragment>
+      <Button
+        onButtonClick={() => setConnectModal(true)}
+        title="Connect to earn 50 UTT"
+        theme="primary"
+        key="twitter-connect"
+      />
 
-    return (
-        <Fragment>
-            <Button onButtonClick={() => setConnectModal(true)} title="Connect to earn 50 UTT" theme="primary"
-                    key="twitter-connect"/>
+      <Modal
+        onAction={() => setConnectModal(false)}
+        onClose={() => setConnectModal(false)}
+        show={connectModal}
+        style={{ maxWidth: 500, minHeight: "60%" }}
+      >
+        <ConnectHelper
+          icon={telegram}
+          title="Allow Access"
+          description="You are about to grant access to UTU"
+        />
 
-            <Modal onClose={() => setConnectModal(false)} show={connectModal}
-                   style={{maxWidth: 500, minHeight: "60%"}}>
+        <label>phone number</label>
+        <input
+          type="text"
+          placeholder="your phone number"
+          value={phoneNumber}
+          onChange={(e) => setPhoneNumber(e.target.value)}
+        />
+        <br />
 
-                <ConnectHelper icon={telegram} title="Allow Access" description="You are about to grant access to UTU"/>
-                <form onSubmit={submitHandler}>
-                    <label>phone number</label>
-                    <input type="text" placeholder="your phone number" value={phoneNumber}
-                           onChange={(e) => setPhoneNumber(e.target.value)}/>
-                    <br/>
+        <label>password</label>
+        <input
+          type="password"
+          placeholder="your password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br />
 
-                    <label>password</label>
-                    <input type="text"
-                           placeholder="your password"
-                           value={password}
-                           onChange={(e) => setPassword(e.target.value)}/>
-                    <br/>
+        <label>code</label>
+        <input
+          type="text"
+          placeholder="sent code"
+          value={phoneCode}
+          onChange={(e) => setPhoneCode(e.target.value)}
+        />
 
-                    <label>code</label>
-                    <input type="text" placeholder="sent code" value={phoneCode}
-                           onChange={(e) => setPhoneCode(e.target.value)}/>
-
-                    <input type="submit" value="Submit"/>
-                </form>
-            </Modal>
-        </Fragment>
-    )
-}
-
+        <button className="tg_btn" onClick={submitHandler}>
+          Submit
+        </button>
+      </Modal>
+    </Fragment>
+  );
+};
 
 export default TelegramConnect;
