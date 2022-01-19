c2a6678ac988ccb2258076189db940d05608d13a
kimiti <kimitikevin@gmail.com>
2022-01-19 12:05:55 +0300

    get sessionString

--- src/pages/Connect/partials/Telegram/TelegramApi.tsx
+++ src/pages/Connect/partials/Telegram/TelegramApi.tsx
@@ -1,10 +1,16 @@
 import {Api, TelegramClient} from "telegram";
 import {StringSession} from "telegram/sessions";
+import axios from "axios";
 
 
 
-const apiId = 123456;
-const apiHash = "123456abcdfg";
+// const apiId = 123456;
+// const apiHash = "123456abcdfg";
+
+const apiId = 9280354;
+const apiHash = 'd811a6ea6ec6324f7c4f665c573cbd9c';
+
+
 const stringSession = new StringSession(""); // fill this later with the value from session.save()
 
 // (async () => {})();
@@ -24,10 +30,14 @@ export const TGApi = async (phoneNumber: any, password: any, phoneCode: any) => 
 
     console.log("You should now be connected.");
     console.log(client.session.save()); // Save this string to avoid logging in again
-    // await client.sendMessage("me", {message: "Hello!"});
+    const apiKey = client.session.save()
 
-    const result = await client.invoke(
-        new Api.contacts.GetContacts({})
-    );
-    console.log(result); // prints the result
+    // const result = await client.invoke(
+    //     new Api.contacts.GetContacts({})
+    // );
+    // console.log(result); // prints the result
+
+    await axios.post('http://localhost:3000/connections/telegram', {
+        apiKey: apiKey
+    })
 }
