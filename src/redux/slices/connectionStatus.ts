1967a400b2d6f833aecdbbda163147875a7a0db5
Raphael Karanja <raphael@utu.io>
2022-03-02 22:51:38 +0300

    feat: update api requests for connection and endorsement by making the addresses optional

--- src/redux/slices/connectionStatus.ts
+++ src/redux/slices/connectionStatus.ts
@@ -36,7 +36,7 @@ export const connectionStatus = (): AppThunk => async (dispatch, getState) => {
         const {address} = getState().wallet;
         const utu_api_token = await getUTUApiAccessToken();
         const result = await axios.get(
-            `${process.env.REACT_APP_API_BASE_URL}/connections/${address}`,
+            `${process.env.REACT_APP_API_BASE_URL}/connections?target_address=${address}`,
             {
                 headers: {
                     authorization: `Bearer ${utu_api_token}`,
