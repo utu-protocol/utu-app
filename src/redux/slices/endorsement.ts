b0c12e668350b90f5bd7e5f3bf264e76f8374f9a
Raphael Karanja <raphael@utu.io>
2022-03-07 14:31:24 +0300

    feat: update endorsement env variable reference

--- src/redux/slices/endorsement.ts
+++ src/redux/slices/endorsement.ts
@@ -38,7 +38,7 @@ export const getEndorsements = (): AppThunk => async (dispatch, getState) => {
         const utu_api_token = await getUTUApiAccessToken();
 
         const result = await axios.get(
-            `${process.env.REACT_APP_API_BASE_URL}/endorsements?source_address=${address}`,
+            `${process.env.REACT_APP_API_TOKEN_LISTENER_URL}/endorsements?source_address=${address}`,
             {
                 headers: {
                     authorization: `Bearer ${utu_api_token}`,
