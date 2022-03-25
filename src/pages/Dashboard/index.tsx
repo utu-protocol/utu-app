babe274969cb3b64f96a2b6517f4523f65327930
musayann <musayann@octangrp.com>
2022-03-25 17:26:36 +0200

    feat: compare addresses in lowercase

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,13 +1,13 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import "./dashboard.scss";
 import TokenCard from "./partials/TokenCard/TokenCard";
 import DetailsCard from "../partials/DetailsCard/DetailsCard";
 import Label from "../../components/Label/Label";
-import {useSelector} from "react-redux";
-import {RootState} from "../../redux/store";
-import {useAppDispatch} from "../../redux/hooks";
-import {getTotalStakedOnYou, getTotalYouStaked, getUttBalance} from "../../redux/slices/balance";
-import {getEndorsements} from "../../redux/slices/endorsement";
+import { useSelector } from "react-redux";
+import { RootState } from "../../redux/store";
+import { useAppDispatch } from "../../redux/hooks";
+import { getTotalStakedOnYou, getTotalYouStaked, getUttBalance } from "../../redux/slices/balance";
+import { getEndorsements } from "../../redux/slices/endorsement";
 
 const Dashboard = () => {
     const {
@@ -18,9 +18,9 @@ const Dashboard = () => {
         total_you_staked_loading,
         staked_on_you_loading
     } = useSelector((state: RootState) => state.balance);
-    const {endorsements, endorsements_loading} = useSelector((state: RootState) => state.endorsement);
-    const {address} = useSelector((state: RootState) => state.wallet);
-
+    const { endorsements, endorsements_loading } = useSelector((state: RootState) => state.endorsement);
+    const { address } = useSelector((state: RootState) => state.wallet);
+    const lowerCaseAddress = String(address).toLowerCase();
     const dispatch = useAppDispatch();
 
     const tokenData = [
@@ -61,7 +61,7 @@ const Dashboard = () => {
         <div className="container-body">
             <div className="cards-container">
                 {
-                    tokenData.map((data, key) => <TokenCard {...data} key={key}/>)
+                    tokenData.map((data, key) => <TokenCard {...data} key={key} />)
                 }
             </div>
 
@@ -73,15 +73,15 @@ const Dashboard = () => {
                     endorsements.length > 0 ?
                         endorsements.map((endorsement, index) =>
                             <DetailsCard key={index}
-                                         title={`${endorsement.source === address ? 'Endorsement issued' : 'Endorsement received'}`}
-                                         description={`${endorsement.source === address ? 'You have issued an endorsement of value ' : 'You received an endorsement of value '} ${endorsement.value}`}
-                                         actions={[<Label key={index}
-                                                          title={`${endorsement.source === address ? '- ' : '+ '} ${endorsement.value}` }
-                                                          theme={`${endorsement.source === address ? 'danger' : 'success'}`}/>]}
+                                title={`${endorsement.source === lowerCaseAddress ? 'Endorsement issued' : 'Endorsement received'}`}
+                                description={`${endorsement.source === lowerCaseAddress ? 'You have issued an endorsement of value ' : 'You received an endorsement of value '} ${endorsement.value}`}
+                                actions={[<Label key={index}
+                                    title={`${endorsement.source === lowerCaseAddress ? '- ' : '+ '} ${endorsement.value}`}
+                                    theme={`${endorsement.source === lowerCaseAddress ? 'danger' : 'success'}`} />]}
                             />)
                         :
                         endorsements_loading ?
-                            <DetailsCard title="" description="" loading={endorsements_loading}/>
+                            <DetailsCard title="" description="" loading={endorsements_loading} />
                             :
                             <div className="empty-now">
                                 No Activities for now!
