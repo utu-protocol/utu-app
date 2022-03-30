18d354e36e06565f1ce50b388fde5df61a075c7a
Raphael Karanja <raphael@utu.io>
2022-03-30 16:20:51 +0300

    refactor: restrict button click when on loading status

--- src/components/Button/UtuButton.tsx
+++ src/components/Button/UtuButton.tsx
@@ -4,14 +4,19 @@ import "./UtuButton.scss";
 interface ButtonProps {
     theme?: string,
     title: string,
-    onButtonClick?: () => void,
+    onButtonClick: () => void,
     loading?: boolean,
-    center?: boolean
+    center?: boolean,
+    skeleton?: boolean
 }
 
 const UtuButton = ({theme, title, onButtonClick, loading = false, center = true}: ButtonProps) => {
+    const handler = () => {
+        return;
+    }
     return (
-        <button className={`utu-btn ${theme} ${center ? 'btn-center ' : ''} `} onClick={onButtonClick}
+        <button className={`utu-btn ${theme} ${center ? 'btn-center ' : ''} `}
+                onClick={loading ? handler : onButtonClick}
                 disabled={loading}>
             {loading ?
                 <div className="utu-btn__spinner">
